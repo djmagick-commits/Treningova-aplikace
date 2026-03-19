ee396619cfd557d3b53fff7fe45c6a5a3a226f5a
djmagick-commits <djmagick@seznam.cz>
2026-03-19 07:03:47 +0100

    zatim jen predbezny rozpis

---
+++ Client.py
@@ -0,0 +1,6 @@
+class Client:
+    def __init__(self, name, age,email,):
+        self.name = name
+        self.age = age
+        self.email = email
+
---
+++ measured.py
@@ -0,0 +1,16 @@
+class Measured:
+    def __init__(self,weight, height,breasts, shoulders, beck, belly, hips, butt, biceps, triceps, thigh, calf, forearms,):
+        self.weight = weight
+        self.height = height
+        self.breasts = breasts
+        self.shoulders = shoulders
+        self.beck = beck
+        self.belly = belly
+        self.hips = hips
+        self.butt = butt
+        self.biceps = biceps
+        self.triceps = triceps
+        self.thigh = thigh
+        self.calf = calf
+        self.forearms = forearms
+
